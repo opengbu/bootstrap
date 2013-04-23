40241224684c781a848e316091b62fa8dd0fdc23
vitalart <theartbi@gmail.com>
2013-04-23 16:14:27 +0300

    Полоса прогресса и новые фаилы

--- js/MyScript.js
+++ js/MyScript.js
@@ -1,15 +1,38 @@
 ﻿
 $(document).ready(function () {
+    $('#button').hover(
+        function(){
+        $('#button').popover('show');
+    },
+        function(){
+        $('#button').popover('hide');
+
+    });
+
+
 
     $('#button').click(function(){
+        $('#button').popover('hide');
+        $('#button').hover(
+            function(){
+                $('#button').popover('hide');
+            },
+            function(){
+                $('#button').popover('hide');
 
-        $('#center').animate({ 'width': '400px', 'height': '220' ,'borderWidth' : '3px'},500);
+            });
+
+        $('#center').css({ 'width': '400px', 'height': '255' ,'borderWidth' : '3px'});
+
         var block = $('<div class="less outL"></div>');
         var lessons = ["Первая", "Вторая", "Третья", "Четвертая", "Пятая", "Шестая"];
+        var progressStriped = $('<div class="bar" style="width: 0%;"></div>');
 
-        function createBlockLessons() {
-
+        function createBlockLessons(s) {
+            var i = s * 10;
+            $('.active').append('<div class="bar" style="width: '+i+'%;"></div>');
             $('.lessons').append(block);
+
             $('.less').attr({"id" : "last"}).animate({marginLeft : '50px',width : '280px'},1000);
             $('#last').clone().attr({"id" : "now"}).appendTo('.lessons');
             $('#now').css({marginLeft : '50px',width : '280px'}).animate({marginLeft : '0px',width : '380px'},1000);
@@ -18,16 +41,50 @@ $(document).ready(function () {
 
         };
 
-        function move(l){
+        function move(l,s){
             $('#last').attr('id', 'dead').slideUp(1000);
             $('#now').attr('id', 'last').animate({marginLeft : "50px", width : "280px"},2000);
             $('#next').attr('id', 'now').animate({marginLeft : "0px", width : "380px"},2000);
             $('#now').clone().attr('id','next').text(lessons[l[2]]).css({display : 'none', width: '380', marginLeft: '0px'}).appendTo('.lessons')
                 .fadeIn(500)
                 .animate({display : 'block', marginLeft: '50px', width : '280px'},1500);
+            $('.bar').remove();
+            $('.active').append(progressStriped);
+            bar(s);
 
         }
-
+        function secondsForBar(s){
+            if([0,10,20,30,40,50].indexOf(s)>=0){
+                return 0;
+            }
+            else if([1,11,21,31,41,51].indexOf(s)>=0){
+                return 1;
+            }
+            else if([2,12,22,32,42,52].indexOf(s)>=0){
+                return 2;
+            }
+            else if([3,13,23,33,43,53].indexOf(s)>=0){
+                return 3;
+            }
+            else if([4,14,24,34,44,54].indexOf(s)>=0){
+                return 4;
+            }
+            else if([5,15,25,35,45,55].indexOf(s)>=0){
+                return 5;
+            }
+            else if([6,16,26,36,46,56].indexOf(s)>=0){
+                return 6;
+            }
+            else if([7,17,27,37,47,57].indexOf(s)>=0){
+                return 7;
+            }
+            else if([8,18,28,38,48,58].indexOf(s)>=0){
+                return 8;
+            }
+            else{
+                return 9;
+            }
+        }
         function less (s){
             var less = [];
             if(s <= 9){
@@ -62,18 +119,29 @@ $(document).ready(function () {
 
         }
 
-        function addL(l){
+        function bar(i){
+
+            $('.bar').css({width : i+'%'});
+
+
+
+        }
+
+        function addL(l,r,s){
           if($('.less').text() == ""){
-              createBlockLessons();
+              createBlockLessons(s);
               $('#last').text(lessons[l[0]]);
               $('#now').text(lessons[l[1]]);
               $('#next').text(lessons[l[2]]);
           }
           else{
               if($('#last').text() != lessons[l[0]]){
-                  move(l);
-
+                  move(l,r);
               }
+             else{
+                  bar(r);
+              }
+
           }
         }
 
@@ -81,15 +149,17 @@ $(document).ready(function () {
             var forTime = new Date();
             var hour = forTime.getHours();
             var minutes = forTime.getMinutes();
-            var seconds = forTime.getSeconds();
+            var s = forTime.getSeconds();
+            var ms = forTime.getMilliseconds();
 
-            var l = less(seconds);
-            addL(l);
-            /*debug.log(l);*/
+            var sec = secondsForBar(s);
+            var r = (sec*1000 + ms)/100;
+            var result = Math.round(r);
+            var l = less(s);
+            addL(l,result,sec);
 
-
-            if(seconds < 10){
-                seconds = "0" + seconds;
+            if(s < 10){
+                s = "0" + s;
             }
             if(minutes < 10){
                 minutes = "0" + minutes;
@@ -97,7 +167,7 @@ $(document).ready(function () {
             if(hour < 10){
                 hour = "0" + hour;
             }
-            $('.time').text(hour + ":" + minutes + ":" + seconds);
+            $('.time').text(hour + ":" + minutes + ":" + s);
         }, 100);
 
 
